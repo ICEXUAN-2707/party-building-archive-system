85edef5526c817302f5d8b3f4b6884e32537213a
ICEXUAN-2707 <3368286501@qq.com>
2026-07-15 11:51:49 +0800

    初始化Django项目配置

---
+++ config/views.py
@@ -0,0 +1,16 @@
+"""Error page handlers."""
+
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+
+def permission_denied(request: HttpRequest, exception: Exception) -> HttpResponse:
+    return render(request, "403.html", status=403)
+
+
+def page_not_found(request: HttpRequest, exception: Exception) -> HttpResponse:
+    return render(request, "404.html", status=404)
+
+
+def server_error(request: HttpRequest) -> HttpResponse:
+    return render(request, "500.html", status=500)
