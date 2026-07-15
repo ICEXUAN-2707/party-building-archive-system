9391ec45a2912b113480ef861a6ee5d11e5246bf
ICEXUAN-2707 <3368286501@qq.com>
2026-07-15 11:52:17 +0800

    添加核心模型和初始化数据命令

---
+++ apps/materials/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+
+from .models import ApplicationRecord, IdeologicalReport, IdeologicalReportSummary
+
+
+@admin.register(ApplicationRecord)
+class ApplicationRecordAdmin(admin.ModelAdmin):
+    list_display = ("student", "applied_at", "updated_at")
+    list_filter = ("applied_at",)
+    search_fields = ("student__name", "student__student_number")
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(IdeologicalReportSummary)
+class IdeologicalReportSummaryAdmin(admin.ModelAdmin):
+    list_display = ("student", "reported_total_count", "calculated_date_count", "updated_at")
+    search_fields = ("student__name", "student__student_number")
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(IdeologicalReport)
+class IdeologicalReportAdmin(admin.ModelAdmin):
+    list_display = ("student", "sequence_number", "submitted_at", "is_active", "created_at")
+    list_filter = ("is_active", "submitted_at")
+    search_fields = ("student__name", "student__student_number", "source_column_name")
+    readonly_fields = ("created_at",)
